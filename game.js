43cc3c55120eebf1a78621be5805d9910a8bb61e
kuanalex <59577267+kuanalex@users.noreply.github.com>
2021-01-20 14:33:28 -0500

    fixed the issue - keyboard -> keypress

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keyboard(function() {
+$(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
